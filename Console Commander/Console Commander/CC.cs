1dc5e8f363deb9dfba483f8261b321c20ecd70d0
Jimmy Cushnie <jimmycushnie@gmail.com>
2018-10-28 17:41:31 -0400

    jfkldsafdsa

---
+++ Console Commander/Console Commander/CC.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Commander
+{
+    /// <summary>
+    /// Color Console
+    /// </summary>
+    static class CC
+    {
+        static void Write(object value, ConsoleColor color = ConsoleColor.Cyan)
+        {
+            var original = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Write(value);
+            Console.ForegroundColor = original;
+        }
+
+        static void WriteLine(object value, ConsoleColor color = ConsoleColor.Cyan)
+        {
+            var original = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(value);
+            Console.ForegroundColor = original;
+        }
+    }
+}
--- Console Commander/Console Commander/Program.cs
+++ Console Commander/Console Commander/Program.cs
@@ -10,6 +10,8 @@ namespace Console_Commander
     {
         static void Main(string[] args)
         {
+
+
         }
     }
 }
