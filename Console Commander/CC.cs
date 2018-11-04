95d57c5a092c1193498f0d65662999cecffd5e16
Jimmy Cushnie <jimmycushnie@gmail.com>
2018-11-04 17:15:31 -0500

    added indents

--- Console Commander/CC.cs
+++ Console Commander/CC.cs
@@ -11,6 +11,8 @@ namespace ConsoleCommander
     /// </summary>
     static class CC
     {
+        public static int IndentInterval = 3;
+
         public static void Write(object value, ConsoleColor color = ConsoleColor.Cyan)
         {
             var original = Console.ForegroundColor;
@@ -19,11 +21,11 @@ namespace ConsoleCommander
             Console.ForegroundColor = original;
         }
 
-        public static void WriteLine(object value, ConsoleColor color = ConsoleColor.Cyan)
+        public static void WriteLine(object value, ConsoleColor color = ConsoleColor.Cyan, int indent = 0)
         {
             var original = Console.ForegroundColor;
             Console.ForegroundColor = color;
-            Console.WriteLine(value);
+            Console.WriteLine(new string(' ', indent * IndentInterval) + value);
             Console.ForegroundColor = original;
         }
 
